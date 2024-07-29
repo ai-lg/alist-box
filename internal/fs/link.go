073069a10a5ac54453c6ab67b7fd5493c2640396
ailg666 <ailg666@hotmail.com>
2024-07-29 13:56:31 +0800

    test link.go

--- internal/fs/link.go
+++ internal/fs/link.go
@@ -9,6 +9,7 @@ import (
 	"github.com/alist-org/alist/v3/server/common"
 	"github.com/gin-gonic/gin"
 	"github.com/pkg/errors"
+	log "github.com/sirupsen/logrus"
 )
 
 func link(ctx context.Context, path string, args model.LinkArgs) (*model.Link, model.Obj, error) {
@@ -16,14 +17,18 @@ func link(ctx context.Context, path string, args model.LinkArgs) (*model.Link, m
 	if err != nil {
 		return nil, nil, errors.WithMessage(err, "failed get storage")
 	}
+	log.Debugf("==========/fs/link.go link() Args before: %+v", args)
 	l, obj, err := op.Link(ctx, storage, actualPath, args)
 	if err != nil {
 		return nil, nil, errors.WithMessage(err, "failed link")
 	}
+	log.Debugf("==========/fs/link.go link() Args after: %+v", args)
+	log.Debugf("==========/fs/link.go link() 1.URL: %+v", l.URL)
 	if l.URL != "" && !strings.HasPrefix(l.URL, "http://") && !strings.HasPrefix(l.URL, "https://") {
 		if c, ok := ctx.(*gin.Context); ok {
 			l.URL = common.GetApiUrl(c.Request) + l.URL
 		}
 	}
+	log.Debugf("==========/fs/link.go link() obj: %+v", obj)
 	return l, obj, nil
 }
